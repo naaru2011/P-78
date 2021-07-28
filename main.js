f1166a00c1bae393849d49cf70be6ea8ac6a53fe
naaru2011 <83050539+naaru2011@users.noreply.github.com>
2021-07-28 13:02:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function updateData(){
+alert("My family has me, my brother Akshu, my mom, my dad, My uncle Shomik, Lovey, Khushee, my aunt Honey, Sonal, My dad's father, my dad's mother, my mom's mother, and my mom's father");
+}
+function info(){
+    alert("My family are indians, and I really like india.");
+}
+function born(){
+    alert("They were all born in Kanpoor, Uttar Pradesh, but I was born in united states.")
+}
